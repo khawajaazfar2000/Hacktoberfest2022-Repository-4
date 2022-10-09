cf3a5a070444e74e46040198db28ae938eb67711
Muhammad Zohaib Arshad <muhammadzohaibarshad18@gmail.com>
2022-10-09 17:35:07 +0500

    even or odd is ok now

---
+++ Check_Even_Or_Odd.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<"Enter the number = ";
+    int a = 0;
+    cin>> a;
+	if(a%2==0)
+	{
+    cout<<"a is even";
+	}
+	else
+	{
+	cout<< "a is odd";
+	}
+    return 0;
+}

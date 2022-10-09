aa37eed2d2fe30c9bb2f285407b473660ea34ddb
khawajaazfar2000 <khawajaazfar2000@gmail.com>
2022-10-09 16:16:17 +0500

    First file

---
+++ Check_Even_Or_Odd_C++.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Enter Number= ";
+    int a;
+    cin>> a;
+   if(a%2==0){
+       cout<<"a is even";}
+   else(cout<< "a is odd");
+
+    return 0;
+}
+

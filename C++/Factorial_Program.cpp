f0e5a0e02f9b9bb8f6b4303c546f8531299b563d
Syed Muhammad Raza Naqvi <55186324+MuhammadRazaNaqvi@users.noreply.github.com>
2022-10-29 23:57:55 +0500

    Update Factorial_Program.cpp
    First commit

--- C++/Factorial_Program.cpp
+++ C++/Factorial_Program.cpp
@@ -4,12 +4,14 @@ using namespace std;
 
 int main()  
 {  
-
+  //we initialize the factorial wih the value of 1 
    int i,fact=1,number;    
   cout<<"Enter any Number: ";    
  cin>>number;    
-  for(i=1;i<=number;i++){    
-      fact=fact*i;    
+  for(i=1;i<=number;i++){   
+      //At each iteration  we multiply the last value of the fact with the vlaue of i
+      // when the condition in 'for' statement is satisfied, the value of factorial is gained
+      fact=fact*i;  
 
   }    
 
